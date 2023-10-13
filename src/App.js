b9720f89dadc84fd6e671cd39e6dd11c7cfeaa15
Kenneth-Mah <kennethmah@rocketmail.com>
2023-10-13 17:47:59 +0800

    Added coffee.svg and changed App.js to change the logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
+import coffee from './coffee.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={coffee} className="App-logo" alt="logo" />
         <p>
           I have edited <code>src/App.js</code> and saved the changes.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://support.google.com/websearch/answer/134479?hl=en"
           target="_blank"
           rel="noopener noreferrer"
         >
